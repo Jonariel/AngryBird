4fce8f8ca9c8b4912d2b00421c0c0a01c72fcba5
Jonariel <jonariel.simon@ocsbstudent.ca>
2020-10-13 20:45:38 -0400

    update Engine

--- sketch.js
+++ sketch.js
@@ -39,6 +39,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    //update engine
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
